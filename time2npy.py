e58faeb5328bb100e484bc4cb3d159571887094f
xaratustrah <shahab@zamail.de>
2014-10-23 11:27:10 +0200

    speeded up the code.

--- time2npy.py
+++ time2npy.py
@@ -10,6 +10,7 @@ import xml.etree.ElementTree as et
 import numpy as np
 import matplotlib.pyplot as plt
 
+verbose = False
 
 def tiq2npy(filename, nframes, lframes, sframes):
     """
@@ -19,70 +20,58 @@ def tiq2npy(filename, nframes, lframes, sframes):
     log.info("File size is {} bytes.".format(filesize))
     filename_wo_ext = os.path.splitext(filename)[0]
     
-    buf = bytearray(b'')
-    ar = np.array([], dtype=complex)
-
-    total_nbytes = 8 * nframes * lframes # 8 comes from 2 times 4 bit integer for I and Q
-    start_nbytes = 8 * (sframes - 1 ) * lframes 
-    global_counter = 0
+    with open (filename) as f:
+        line = f.readline()
+    data_offset = int(line.split("\"")[1])
 
     with open(filename, 'rb') as f:
-        byte = f.read(1)
-        global_counter += 1
-        while byte != b'':
-            buf += byte
-            bufstr = buf.decode('utf-8')
-            if (bufstr.endswith('</DataFile>')) :
-                log.info("Found end of header section.")
-                break
-            byte = f.read(1)
-            global_counter += 1
+        ba = f.read(data_offset)
 
-        xmltree = et.fromstring(bufstr)
-        for elem in xmltree.iter(tag='{http://www.tektronix.com}Frequency'):
-            center=float(elem.text)
-        for elem in xmltree.iter(tag='{http://www.tektronix.com}MaxSpan'):
-            span=float(elem.text)
-        for elem in xmltree.iter(tag='{http://www.tektronix.com}Scaling'):
-            scale=float(elem.text)
-        for elem in xmltree.iter(tag='{http://www.tektronix.com}SamplingFrequency'):
-            fs=float(elem.text)
-        log.info("Center {0} Hz, span {1} Hz, sampling frequency {2} scale factor {3}.".format(center, span, fs, scale))
-        log.info("Header size {} bytes.".format(global_counter))
+    xmltree = et.fromstring(ba)
+    for elem in xmltree.iter(tag='{http://www.tektronix.com}Frequency'):
+        center=float(elem.text)
+    for elem in xmltree.iter(tag='{http://www.tektronix.com}MaxSpan'):
+        span=float(elem.text)
+    for elem in xmltree.iter(tag='{http://www.tektronix.com}Scaling'):
+        scale=float(elem.text)
+    for elem in xmltree.iter(tag='{http://www.tektronix.com}SamplingFrequency'):
+        fs=float(elem.text)
+
+    log.info("Center {0} Hz, span {1} Hz, sampling frequency {2} scale factor {3}.".format(center, span, fs, scale))
+    log.info("Header size {} bytes.".format(data_offset))
+
+    with open (filename_wo_ext + '.xml', 'wb') as f3 : 
+        f3.write(ba)
+    log.info("Header saved in an xml file.")
         
-        with open (filename_wo_ext + '.xml', 'w') as f3 : f3.write(bufstr)
-        log.info("Header saved in an xml file.")
-        
-        log.info("Proceeding to read binary section, 32bit (4 byte) little endian.")
+    total_nbytes = 8 * nframes * lframes # 8 comes from 2 times 4 byte integer for I and Q
+    start_nbytes = 8 * (sframes - 1 ) * lframes
+    log.info("Proceeding to read binary section, 32bit (4 byte) little endian.")
+    log.info("Total number of frames: {0} = {1}s".format((filesize-data_offset)/8/lframes, (filesize-data_offset)/8/fs))
+    log.info("Frame length: {0} data points = {1}s".format(lframes, lframes/fs))
+    log.info("Frame offset: {0} = {1}s".format(sframes, start_nbytes/fs))
+    log.info("Reading {0} frames = {1}s.".format(nframes, total_nbytes/fs))
 
-        global_counter = start_nbytes              # reset the global counter
-        f.read(start_nbytes)                       # move the pointer
-        ba = f.read(4)
-        global_counter += 4
-        
-        while ba != b'':
-            I = int.from_bytes(ba, byteorder = 'little')
+    with open (filename, 'rb') as f:
+        f.seek(data_offset + start_nbytes)
+        ba = f.read(total_nbytes)
 
-            ba = f.read(4)
-            global_counter += 4
-            Q = int.from_bytes(ba, byteorder = 'little')
+    ar = np.array([], dtype=complex)
+    for i in range (0, len(ba), 8):
+        I = int.from_bytes(ba[i:i+4], byteorder = 'little')
+        Q = int.from_bytes(ba[i+4:i+8], byteorder = 'little')
+        ar = np.append(ar, scale * complex(I, Q))
+        if verbose :
+            sys.stdout.write('\rProgress: ' + str(int(i*100/len(ba)))+'%')
+            sys.stdout.flush()
 
-            ar = np.append(ar, scale * complex(I, Q))
-                
-            if (global_counter >= total_nbytes - 1) : break
-            else :
-                ba = f.read(4)
-                global_counter += 4
-                
-                sys.stdout.flush()
-                sys.stdout.write('\rProgress: ' + str(int(global_counter*100/total_nbytes))+'%')
-    print('\n')
+    if verbose : print('\n')
     log.info("Output complex array has a size of {}.".format(ar.size))
-    dic = {'center': center, 'span': span, 'fs': fs, 'scale':scale, 'data': ar}
+    dic = {'center': center, 'span': span, 'fs': fs, 'data': ar}
     np.save(filename_wo_ext + '.npy', dic)
-
     # in order to read use: data = x.item()['data'] or data = x[()]['data'] other wise you get 0-d error
-        
+
+
 import argparse
 import logging as log
 
@@ -97,11 +86,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.verbose:
         log.basicConfig(level=log.DEBUG)
+        verbose = True
         
     nframes = 10 if not args.nframes else args.nframes
     lframes = 1024 if not args.lframes else args.lframes
     sframes = 1 if not args.sframes else args.sframes
     
-    log.info("File {0} passed for processing.".format(args.filename))
+    log.info("File {} passed for processing.".format(args.filename))
+    
     tiq2npy(args.filename, nframes, lframes, sframes)
 
