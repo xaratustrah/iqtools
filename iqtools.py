147662346adcdd9e08d4c562eadad3ca7c12b15c
xaratustrah <shahab@zamail.de>
2015-04-21 11:34:31 +0200

    object oriented version pushed.

--- iqtools.py
+++ iqtools.py
@@ -20,6 +20,180 @@ from scipy.io import wavfile
 
 verbose = False
 
+# ------------- BEGIN OF CLASS DEFINITION ------------- #
+
+
+class IQData(object):
+    def __init__(self):
+        self.filename = ''
+        self.filename_wo_ext = ''
+        self.acq_bw = 0.0
+        self.center = 0.0
+        self.date_time = ''
+        self.number_samples = 0
+        self.rbw = 0.0
+        self.rf_att = 0.0
+        self.fs = 0.0
+        self.span = 0.0
+        self.scale = 0.0
+        self.n_frames_tot = 0
+        self.dictionary = {}
+        self.header = None
+        self.data_array = None
+        self.lframes = 0
+        return
+
+    def read_iqt(self, filename):
+        # todo: to be done
+        return None
+
+
+    def read_tdms(self, filename, meta_filename, nframes=0, lframes=0, sframes=0):
+        """Some good fried will continue here"""
+
+        # todo: returns a dictionary containing info e.g. complex array (c16), sampling rate etc...
+        return None
+
+
+    def read_tiq(self, filename, nframes=10, lframes=1024, sframes=1):
+        """Process the tiq input file.
+        Following information are extracted, except Data offset, all other are stored in the dic. Data needs to be normalized over 50 ohm.
+
+        AcquisitionBandwidth
+        Frequency
+        File name
+        Data I and Q [Unit is Volt]
+        Data_offset
+        DateTime
+        NumberSamples
+        Resolution Bandwidth
+        RFAttenuation (it is already considered in the data scaling, no need to use this value, only for info)
+        Sampling Frequency
+        Span
+        Voltage Scaling
+        """
+
+        self.lframes = lframes
+        self.filename = filename
+        self.filename_wo_ext = os.path.splitext(filename)[0]
+
+        filesize = os.path.getsize(filename)
+        log.info("File size is {} bytes.".format(filesize))
+
+        with open(filename) as f:
+            line = f.readline()
+        data_offset = int(line.split("\"")[1])
+
+        with open(filename, 'rb') as f:
+            ba = f.read(data_offset)
+
+        xml_tree_root = et.fromstring(ba)
+
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}AcquisitionBandwidth'):
+            self.acq_bw = float(elem.text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}Frequency'):
+            self.center = float(elem.text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}DateTime'):
+            self.date_time = str(elem.text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}NumberSamples'):
+            self.number_samples = int(elem.text)  # this entry matches (filesize - data_offset) / 8) well
+        for elem in xml_tree_root.iter('NumericParameter'):
+            if 'name' in elem.attrib and elem.attrib['name'] == 'Resolution Bandwidth' and elem.attrib['pid'] == 'rbw':
+                self.rbw = float(elem.find('Value').text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}RFAttenuation'):
+            self.rf_att = float(elem.text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}SamplingFrequency'):
+            self.fs = float(elem.text)
+        for elem in xml_tree_root.iter('NumericParameter'):
+            if 'name' in elem.attrib and elem.attrib['name'] == 'Span' and elem.attrib['pid'] == 'globalrange':
+                self.span = float(elem.find('Value').text)
+        for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}Scaling'):
+            self.scale = float(elem.text)
+
+        log.info("Center {0} Hz, span {1} Hz, sampling frequency {2} scale factor {3}.".format(self.center, self.span,
+                                                                                               self.fs, self.scale))
+        log.info("Header size {} bytes.".format(data_offset))
+
+        log.info("Proceeding to read binary section, 32bit (4 byte) little endian.")
+        log.info('Total number of samples: {}'.format(self.number_samples))
+        log.info("Frame length: {0} data points = {1}s".format(lframes, lframes / self.fs))
+        self.n_frames_tot = int(self.number_samples / lframes)
+        log.info("Total number of frames: {0} = {1}s".format(self.n_frames_tot, self.number_samples / self.fs))
+        log.info("Start reading at offset: {0} = {1}s".format(sframes, sframes * lframes / self.fs))
+        log.info("Reading {0} frames = {1}s.".format(nframes, nframes * lframes / self.fs))
+
+        self.header = ba
+
+        total_n_bytes = 8 * nframes * lframes  # 8 comes from 2 times 4 byte integer for I and Q
+        start_n_bytes = 8 * (sframes - 1) * lframes
+
+        with open(filename, 'rb') as f:
+            f.seek(data_offset + start_n_bytes)
+            ba = f.read(total_n_bytes)
+
+        # return a numpy array of little endian 8 byte floats (known as doubles)
+        self.data_array = np.fromstring(ba, dtype='<i4')  # little endian 4 byte ints.
+        # Scale to retrieve value in Volts. Augmented assignment does not work here!
+        self.data_array = self.data_array * self.scale
+        self.data_array = self.data_array.view(
+            dtype='c16')  # reinterpret the bytes as a 16 byte complex number, which consists of 2 doubles.
+
+        log.info("Output complex array has a size of {}.".format(self.data_array.size))
+        self.dictionary = {'center': self.center, 'number_samples': self.number_samples, 'fs': self.fs,
+                           'lframes': self.lframes, 'data': self.data_array,
+                           'nframes_tot': self.n_frames_tot, 'DateTime': self.date_time, 'rf_att': self.rf_att,
+                           'span': self.span,
+                           'acq_bw': self.acq_bw,
+                           'file_name': self.filename, 'rbw': self.rbw}
+
+        # in order to read you may use: data = x.item()['data'] or data = x[()]['data'] other wise you get 0-d error
+        return self.dictionary, self.header
+
+
+    def save_header(self):
+        """Saves the header byte array into a txt tile."""
+        with open(self.filename_wo_ext + '.xml', 'wb') as f3:
+            f3.write(self.header)
+        log.info("Header saved in an xml file.")
+
+
+    def save_data(self):
+        """Saves the dictionary to a numpy file."""
+        np.save(self.filename_wo_ext + '.npy', self.dictionary)
+
+
+    def save_audio(self, afs):
+        """ Save the singal as an audio wave """
+        wavfile.write(self.filename_wo_ext + '.wav', afs, abs(self.data_array))
+
+
+    @staticmethod
+    def read_result_csv(filename):
+        p = np.genfromtxt(filename, skip_header=63)
+        with open(filename) as f:
+            cont = f.readlines()
+        for l in cont:
+            l = l.split(',')
+            if 'Frequency' in l and len(l) == 3:
+                center = float(l[1])
+            if 'XStart' in l and len(l) == 3:
+                start = float(l[1])
+            if 'XStop' in l and len(l) == 3:
+                stop = float(l[1])
+        f = np.linspace(start - center, stop - center, len(p))
+        return f, p
+
+
+    @staticmethod
+    def read_data_csv(filename):
+        """Reads the CSV data export from the instrument. Please note that 50 ohm power termination is already considered for these data.
+        """
+        data = np.genfromtxt(filename, skip_header=10, delimiter=",")
+        data = np.ravel(data).view(dtype='c16')  # has one dimension more, should use ravel
+        return data
+
+
+# ------------- END OF CLASS DEFINITION ------------- #
 
 def make_signal(f, fs, l=1, nharm=0, noise=True):
     """Make a sine signal with/without noise."""
@@ -34,17 +208,6 @@ def make_signal(f, fs, l=1, nharm=0, noise=True):
     return t, x
 
 
-def make_analytical(x):
-    """Make an analytical signal from the real signal"""
-
-    yy = hilbert(x)
-    ii = np.real(yy)
-    qq = np.imag(yy)
-    x_bar = np.vectorize(complex)(ii, qq)
-    ins_ph = np.angle(x_bar) * 180 / np.pi
-    return x_bar, ins_ph
-
-
 def plot_hilbert(x_bar):
     """Show Hilbert plot."""
 
@@ -54,14 +217,18 @@ def plot_hilbert(x_bar):
     plt.ylabel('Imag. Part')
 
 
-def channel_power_dbm(f, p_avg):
+def get_channel_power_dbm(f, p_avg):
     """ Return total power in band in dBm
     Input: average power in Watts
     """
     return get_dbm(np.trapz(p_avg, x=f))
 
 
-def plot_dbm_per_hz(f, p, cen=0, span=None, filename='', to_file=False):
+def get_dbm(watt):
+    return 10 * np.log10(watt * 1000)
+
+
+def plot_dbm_per_hz(f, p, cen=0.0, span=None, filename='', to_file=False):
     """Plot average power in dBm per Hz"""
 
     if not span:
@@ -92,36 +259,6 @@ def get_fft_50_ohm(x, fs):
 def get_pwelch(x, fs):
     f, p_avg = welch(x, fs, nperseg=1024)
     return np.fft.fftshift(f), np.fft.fftshift(p_avg)
-
-
-def filename_wo_ext(filename):
-    """Extracts the filename base"""
-
-    return os.path.splitext(filename)[0]
-
-
-def save_header(filename, ba):
-    """Saves the header byte array into a txt tile."""
-
-    with open(filename + '.xml', 'wb') as f3:
-        f3.write(ba)
-    log.info("Header saved in an xml file.")
-
-
-def save_data(filename, dic):
-    """Saves the dictionary to a numpy file."""
-
-    np.save(filename + '.npy', dic)
-
-
-def save_audio(filename, afs, na):
-    """ Save the singal as an audio wave """
-
-    wavfile.write(filename_wo_ext(filename) + '.wav', afs, abs(na))
-
-
-def get_dbm(watt):
-    return 10 * np.log10(watt * 1000)
 
 
 def get_watt(dbm):
@@ -164,137 +301,26 @@ def get_fwhm(f, p):
     return [f_m3db, f_p3db], [p_m3db, p_p3db]
 
 
-def zoom_in_freq(f, p, center=0, span=1000):
-    low = center - span / 2
-    high = center + span / 2
-    mask = (f > low) & (f < high)
-    return f[mask], p[mask]
+def make_analytical(x):
+    """Make an analytical signal from the real signal"""
+
+    yy = hilbert(x)
+    ii = np.real(yy)
+    qq = np.imag(yy)
+    x_bar = np.vectorize(complex)(ii, qq)
+    ins_ph = np.angle(x_bar) * 180 / np.pi
+    return x_bar, ins_ph
 
 
 def shift_to_center(f, center):
     return center + f
 
 
-def read_result_csv(filename):
-    p = np.genfromtxt(filename, skip_header=63)
-    with open(filename) as f:
-        cont = f.readlines()
-    for l in cont:
-        l = l.split(',')
-        if 'Frequency' in l and len(l) == 3:
-            center = float(l[1])
-        if 'XStart' in l and len(l) == 3:
-            start = float(l[1])
-        if 'XStop' in l and len(l) == 3:
-            stop = float(l[1])
-    f = np.linspace(start - center, stop - center, len(p))
-    return f, p
-
-
-def read_data_csv(filename):
-    """Reads the CSV data export from the instrument. Please note that 50 ohm power termination is already considered for these data.
-    """
-    data = np.genfromtxt(filename, skip_header=10, delimiter=",")
-    data = np.ravel(data).view(dtype='c16')  # has one dimension more, should use ravel
-    return data
-
-
-def read_tdms(data_filename, meta_filename, nframes=0, lframes=0, sframes=0):
-    """Some good fried will continue here"""
-
-    # todo: returns a dictionary containing info e.g. complex array (c16), sampling rate etc...
-    return None
-
-
-def read_iqt(filename):
-    # todo: to be done
-    return None
-
-
-def read_tiq(filename, nframes=10, lframes=1024, sframes=1):
-    """Process the tiq input file.
-    Following information are extracted, except Data offset, all other are stored in the dic. Data needs to be normalized over 50 ohm.
-
-    AcquisitionBandwidth
-    Frequency
-    File name
-    Data I and Q [Unit is Volt]
-    Data_offset
-    DateTime
-    NumberSamples
-    Resolution Bandwidth
-    RFAttenuation (it is already considered in the data scaling, no need to use this value, only for info)
-    Sampling Frequency
-    Span
-    Voltage Scaling
-    """
-
-    filesize = os.path.getsize(filename)
-    log.info("File size is {} bytes.".format(filesize))
-
-    with open(filename) as f:
-        line = f.readline()
-    data_offset = int(line.split("\"")[1])
-
-    with open(filename, 'rb') as f:
-        ba = f.read(data_offset)
-
-    xml_tree_root = et.fromstring(ba)
-    rbw = None
-
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}AcquisitionBandwidth'):
-        acq_bw = float(elem.text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}Frequency'):
-        center = float(elem.text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}DateTime'):
-        date_time = str(elem.text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}NumberSamples'):
-        number_samples = int(elem.text)  # this entry matches (filesize - data_offset) / 8) well
-    for elem in xml_tree_root.iter('NumericParameter'):
-        if 'name' in elem.attrib and elem.attrib['name'] == 'Resolution Bandwidth' and elem.attrib['pid'] == 'rbw':
-            rbw = float(elem.find('Value').text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}RFAttenuation'):
-        rf_att = float(elem.text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}SamplingFrequency'):
-        fs = float(elem.text)
-    for elem in xml_tree_root.iter('NumericParameter'):
-        if 'name' in elem.attrib and elem.attrib['name'] == 'Span' and elem.attrib['pid'] == 'globalrange':
-            span = float(elem.find('Value').text)
-    for elem in xml_tree_root.iter(tag='{http://www.tektronix.com}Scaling'):
-        scale = float(elem.text)
-
-    log.info("Center {0} Hz, span {1} Hz, sampling frequency {2} scale factor {3}.".format(center, span, fs, scale))
-    log.info("Header size {} bytes.".format(data_offset))
-
-    log.info("Proceeding to read binary section, 32bit (4 byte) little endian.")
-    log.info('Total number of samples: {}'.format(number_samples))
-    log.info("Frame length: {0} data points = {1}s".format(lframes, lframes / fs))
-    nframes_tot = int(number_samples / lframes)
-    log.info("Total number of frames: {0} = {1}s".format(nframes_tot, number_samples / fs))
-    log.info("Start reading at offset: {0} = {1}s".format(sframes, sframes * lframes / fs))
-    log.info("Reading {0} frames = {1}s.".format(nframes, nframes * lframes / fs))
-
-    head = ba
-
-    total_nbytes = 8 * nframes * lframes  # 8 comes from 2 times 4 byte integer for I and Q
-    start_nbytes = 8 * (sframes - 1) * lframes
-
-    with open(filename, 'rb') as f:
-        f.seek(data_offset + start_nbytes)
-        ba = f.read(total_nbytes)
-
-    # return a numpy array of little endian 8 byte floats (known as doubles)
-    ar = np.fromstring(ba, dtype='<i4')  # little endian 4 byte ints.
-    ar = ar * scale  # Scale to retreive value in Volts. Augmented assigment does not work here.
-    ar = ar.view(dtype='c16')  # reinterpret the bytes as a 16 byte complex number, which consists of 2 doubles.
-
-    log.info("Output complex array has a size of {}.".format(ar.size))
-    dict_local = {'center': center, 'number_samples': number_samples, 'fs': fs, 'lframes': lframes, 'data': ar,
-                  'nframes_tot': nframes_tot, 'DateTime': date_time, 'rf_att': rf_att, 'span': span, 'acq_bw': acq_bw,
-                  'file_name': filename, 'rbw': rbw}
-
-    # in order to read you may use: data = x.item()['data'] or data = x[()]['data'] other wise you get 0-d error
-    return dict_local, head
+def zoom_in_freq(f, p, center=0, span=1000):
+    low = center - span / 2
+    high = center + span / 2
+    mask = (f > low) & (f < high)
+    return f[mask], p[mask]
 
 
 if __name__ == "__main__":
@@ -320,26 +346,27 @@ if __name__ == "__main__":
 
     log.info("File {} passed for processing.".format(args.filename))
 
-    dic, header = read_tiq(args.filename, args.nframes, args.lframes, args.sframes)
+    iq_data = IQData()
+    _, _ = iq_data.read_tiq(args.filename, args.nframes, args.lframes, args.sframes)
 
     if args.fft:
         log.info('Generating FFT plot.')
-        f1, v1, p1 = get_fft_50(dic['data'], dic['fs'])
-        plot_dbm_per_hz(f1, p1, dic['center'], dic['span'], filename_wo_ext(args.filename) + '_psd_fft', True)
+        f1, v1, p1 = get_fft_50_ohm(iq_data.data_array, iq_data.fs)
+        plot_dbm_per_hz(f1, p1, iq_data.center, iq_data.span, iq_data.filename_wo_ext + '_psd_fft', True)
 
     if args.psd:
         log.info('Generating PSD plot.')
-        f2, p2 = get_pwelch(dic['data'], dic['fs'])
-        plot_dbm_per_hz(f2, p2, dic['center'], dic['span'], filename_wo_ext(args.filename) + '_psd_welch', True)
+        f2, p2 = get_pwelch(iq_data.data_array, iq_data.fs)
+        plot_dbm_per_hz(f2, p2, iq_data.center, iq_data.span, iq_data.filename_wo_ext + '_psd_welch', True)
 
     if args.xml:
         log.info('Saving header into xml file.')
-        save_header(filename_wo_ext(args.filename), header)
+        iq_data.save_header()
 
     if args.npy:
         log.info('Saving data dictionary in numpy format.')
-        save_data(filename_wo_ext(args.filename), dic)
+        iq_data.save_data()
 
     if args.dic:
         log.info('Printing dictionary on the screen.')
-        pprint(dic)
+        pprint(iq_data.dictionary)
