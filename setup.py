92f196e193dcb1a3f8f75ed22fe6d10302bde523
xaratustrah <shahab.sanjari@gmail.com>
2022-02-22 18:28:06 +0100

    setup.py fixed

--- setup.py
+++ setup.py
@@ -1,28 +1,20 @@
 # PyPI setup file
-# thanks to https://stackoverflow.com/a/23265673/5177935
-#
 
 from setuptools import setup, find_packages
-from iqtools.version import __version__
+from barion.version import __version__
+from pathlib import Path
 
-long_description = ''
-
-try:
-    from pypandoc import convert
-
-    read_md = lambda f: convert(f, 'rst', 'md')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 classifiers = [
     'Environment :: Console',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Natural Language :: English',
     'Operating System :: OS Independent',
-    'Topic :: Scientific/Engineering :: Physics'
+    'Programming Language :: Python :: 3 :: Only',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Intended Audience :: Science/Research',
 
 ]
 
@@ -31,10 +23,12 @@ setup(
     packages=find_packages(),
     version=__version__,
     description='Collection of tools for dealing with in phase / quadrature time series data.',
-    long_description=read_md('README.md'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Xaratustra',
     url='https://github.com/xaratustrah/iqtools',  # use the URL to the github repo
-    download_url='https://github.com/xaratustrah/iqtools/tarball/{}'.format(__version__),
+    download_url='https://github.com/xaratustrah/iqtools/tarball/{}'.format(
+        __version__),
     entry_points={
         'console_scripts': [
             'iqtools = iqtools.__main__:main'
